3165d0d2b412eaa396eee2c8325ba1eaa22a255c
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2026-03-20 12:05:11 -0700

    1st half done.

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 # 1. Write a function `make_dictionary` that takes two lists and returns a
 # dictionary with the names as keys and the scores as values.
-def make_dictionary(keys_list, values_list):
-    """
-    turn 2 lists into a dictionary
-    
-    Here are some tests. Make sure your code can handle these cases!
-    >>> make_dictionary(["a", "b"], [1, 2])
-    {'a': 1, 'b': 2}
-    >>> make_dictionary([1, 2, 3], [5, 6, 7])
-    {1:5, 2: 6, 3: 7}
-    >>> make_dictionary(["a", "b", "c", "b"], ["apple", "banana", "clementine", "date"])
-    {'a': 'apple', 'b': 'banana', 'c': 'clementine', 'd': 'date'}
-    >>> make_dictionary(["key"], ["value"])
-    {'key': 'value'}
-    """
 
-    return #### YOUR CODE HERE
+def make_dictionary(keys_list, values_list):
+    new_dict = {}
+    for i in range(len(keys_list)):
+        key = keys_list[i]
+        value = values_list[i]
+        if key in new_dict:
+            new_dict[key] += value
+        else:
+            new_dict[key] = value
+
+    #new_dict[keys_list] = values_list
+
+    return new_dict
 
 print(make_dictionary(["a", "b"], [1, 2]))
 print(make_dictionary([1, 2, 3], [5, 6, 7]))
 print(make_dictionary(["a", "b", "c", "b"], ["apple", "banana", "clementine", "date"]))
 print(make_dictionary(["key"], ["value"]))
+
+
+
+
+
+
+
+
+
+
 
 # You have been given the following list fo students and their test scores:
 names = ["Maria", "Nushi", "Mohammed", "Jose", "Wei"]
