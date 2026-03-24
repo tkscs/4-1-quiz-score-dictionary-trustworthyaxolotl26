604e04ddc3ffe7873487786f31900eaffb6169a7
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2026-03-24 12:31:11 -0700

    i think this works

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ def make_dictionary(keys_list, values_list):
             new_dict[key] += value
         else:
             new_dict[key] = value
-
     #new_dict[keys_list] = values_list
-
     return new_dict
 
-print(make_dictionary(["a", "b"], [1, 2]))
-print(make_dictionary([1, 2, 3], [5, 6, 7]))
-print(make_dictionary(["a", "b", "c", "b"], ["apple", "banana", "clementine", "date"]))
-print(make_dictionary(["key"], ["value"]))
+# print(make_dictionary(["a", "b"], [1, 2]))
+# print(make_dictionary([1, 2, 3], [5, 6, 7]))
+# print(make_dictionary(["a", "b", "c", "b"], ["apple", "banana", "clementine", "date"]))
+# print(make_dictionary(["key"], ["value"]))
 
 
 
@@ -39,17 +37,27 @@ scores = [10, 23, 13, 18, 12]
 score_dict = make_dictionary(names, scores)
 
 # 2. Using `score_dict`, find the score for "Nushi"
-#### YOUR CODE HERE
+print(score_dict["Nushi"])
 
 # 3. Add a score 19 for "John"
-#### YOUR CODE HERE
+score_dict["John"] = 19
+scores.append(19)
 
 # 4. Calculate the average of all the scores in `score_dict`
-#### YOUR CODE HERE
+##### pull scores, add together, divide 
+y = 0
+for i in scores:
+    y += i
+x = y/len(scores)
+print(x)
+
+
 
 # 5. Update the score for "Wei" to be 13
-#### YOUR CODE HERE
+score_dict["Wei"] = 13
 
 # 6. Nushi has just dropped this class. Delete "Nushi" and his score from
 # `score_dict`
-#### YOUR CODE HERE
+del score_dict["Nushi"]
+
+print(score_dict)
